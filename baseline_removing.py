8880c1b34fad59d1cc51e0e693c38d19f68e5b41
MKujawa1 <maciej.kujawa96@o2.pl>
2022-06-07 20:42:28 +0200

    Code after cleaning

--- baseline_removing.py
+++ baseline_removing.py
@@ -4,108 +4,101 @@ import matplotlib.pyplot as plt
 import random
 from scipy.optimize import curve_fit
 
-def lorentzian_gen( x, x0, a, gam ):
+def lorentzian (x, x0, a, gam):
     return a * gam**2 / ( gam**2 + ( x - x0 )**2)
 
-def generate_data(noi=1,scale = 1):
-    
-    l = 2000
-    
-    x2 = np.linspace(-l//2,l//2,l)
+def generate_data(noi = 1,scale = 1):
+    '''
+    Genetaring lorentzian type signal with noise
 
+    Parameters
+    ----------
+    noi : float, optional
+        amplitude of noise. The default is 1.
+    scale : float, optional
+        scaling result. The default is 1.
 
-    end_lor = lorentzian_gen(
-        x2, np.random.randint(-400,400,1)[0], 
+    Returns
+    -------
+    1d array
+        returns lorentzian signal with noise.
+
+    '''
+    size = 2000
+    x = np.linspace(-size//2,size//2,size)
+    
+    spect = lorentzian(
+        x, np.random.randint(-400,400,1)[0], 
         np.random.randint(10,60,1)[0], 
         np.random.randint(60,200,1)[0])
-        
-   
-    wave = end_lor
-   
-    end_noi = 0
-    for v in range(3):
-        end_noi = end_noi+ np.random.randn(l)*noi
-    end_noi = end_noi/3
-   
-    data = wave+end_noi
+    
+    noise = 0
+    for _ in range(3):
+        noise = noise+(np.random.randn(size)*noi)-(noi/2)
+    noise = noise/3
+    data = spect+noise
     data = data-(np.mean(data[:5]))
     return data*scale
     
-
-def lorentzian_gen3( x, x0, a, gam ,a2,freq,dc,phase,a3,freq2,dc2,phase2):
-
+def lorentzian_wave(x, x0, a, gam ,a2,freq,dc,phase,a3,freq2,dc2,phase2):
     return a * gam**2 / ( gam**2 + ( x - x0 )**2)+\
         (a2*np.sin(2*np.pi*(x)*freq/len(x)+math.radians(phase))+dc)+\
             (a3*np.cos(2*np.pi*(x)*freq2/len(x)+math.radians(phase2))+dc2)
            
-
-def lorentzian_gen2( x, x0, a, gam):
-    return a * gam**2 / ( gam**2 + ( x - x0 )**2)
-
-def wave(x,a2,freq,dc,phase,
-         a3,freq2,dc2,phase2):
-    return (a2*np.sin(2*np.pi*(x)*freq/len(x)+math.radians(phase))+dc)+\
-        (a3*np.cos(2*np.pi*(x)*freq2/len(x)+math.radians(phase2))+dc2)
-       
-
+def wave(x,a,freq,dc,phase,a2,freq2,dc2,phase2):
+    return (a*np.sin(2*np.pi*(x)*freq/len(x)+math.radians(phase))+dc)+\
+        (a2*np.cos(2*np.pi*(x)*freq2/len(x)+math.radians(phase2))+dc2)
+### Params to fit
+scale = 1
+pause = 0.5 
+x0 = [300,1700] 
+lor_amp = [3,200]
+gam = [10,300]
+wave_amp = [0.5,100]
+freq = [0.01,1.2]
+dc = [-100,100]
+phase = [0,360]
+### Params to generate lorentz
+gen_amp = [0.5, 3]
+gen_scale = [0.3,2]
+### Params to generate baseline
+rand_amp = [3,6]
+rand_freq = [0.5, 1.3]
+rand_ph = [20,340]
+rand_dc = [-20,20]
+### Creating baseline, fitting, removing baseline, results on plot in loop     
 plt.figure(figsize = (12,4))
 while True:
-    data = generate_data(2,0.5)
-    
+    ### Generate lorentz
+    data = generate_data(random.uniform(gen_amp[0],gen_amp[1]),random.uniform(gen_scale[0], gen_scale[1]))
     x = np.arange(len(data))
-    d = random.uniform(4, 6)*np.sin(2*np.pi*(x)*random.uniform(0.5, 1.3)/len(x)+math.radians(random.uniform(0, 360)))+random.uniform(-10, 10)+\
-        (random.uniform(4, 6)*np.sin(2*np.pi*(x)*random.uniform(0.5, 1.3)/len(x)+math.radians(random.uniform(0, 360)))+random.uniform(-10, 10))
-    
-    data_bl = data+d*1
-    
-    
-    f3,p3 = curve_fit(lorentzian_gen3, x, data_bl,
-                    bounds=([300,3,10,
-                             0.5,0.01,-100,0,
-                             0.5,0.01,-100,0],
-                            [1700,200,300,
-                             100,1.2,100,360,
-                             100,1.2,100,360]))
-    
-    
-    fun3 = lorentzian_gen3(x,*f3)
-    
-    # plt.plot(data_bl)
-    # plt.plot(fun3)
+    ### Generate baseline with sin and cos
+    baseline = random.uniform(rand_amp[0],rand_amp[1])*np.sin(2*np.pi*(x)*\
+            random.uniform(rand_freq[0],rand_freq[1])/len(x)+\
+                math.radians(random.uniform(rand_ph[0],rand_ph[1])))+random.uniform(rand_dc[0],rand_dc[1])+\
+        random.uniform(rand_amp[0],rand_amp[1])*np.cos(2*np.pi*(x)*\
+            random.uniform(rand_freq[0],rand_freq[1])/len(x)+\
+                math.radians(random.uniform(rand_ph[0],rand_ph[1])))+random.uniform(rand_dc[0],rand_dc[1])
+    ### Adding baseline to data
+    data_bl = data+baseline*scale
+    ### Fit to data with baseline
+    fit,p = curve_fit(lorentzian_wave, x, data_bl,
+                    bounds=([x0[0],lor_amp[0],gam[0],
+                             wave_amp[0],freq[0],dc[0],phase[0],
+                             wave_amp[0],freq[0],dc[0],phase[0]],
+                            [x0[1],lor_amp[1],gam[1],
+                             wave_amp[1],freq[1],dc[1],phase[1],
+                             wave_amp[1],freq[1],dc[1],phase[1]]))
+    ### Baseline based on previous fit
+    bl_rem = wave(x,*fit[-8:])
+    ### Removing baseline and displaying plots
     plt.clf()
-    w = wave(x,*f3[-8:])
     plt.subplot(1,2,1)
-    plt.plot(data_bl)
-    plt.plot((w+(np.mean(data_bl[:5])-np.mean(w[:5]))))
-    
-    
+    plt.plot(data_bl,label = 'data with baseline')
+    plt.plot((bl_rem+(np.mean(data_bl[:5])-np.mean(bl_rem[:5]))),label = 'fit baseline')
+    plt.legend()
     plt.subplot(1,2,2)
-    
-    plt.plot(data)
-    plt.plot(data_bl-(w+(np.mean(data_bl[:5])-np.mean(w[:5]))))
-    
-    # l = lorentzian_gen2(x,f3[0],f3[1],f3[2])
-    
-    # bl = fun3-l
-    
-    # # plt.plot(data_bl)
-    # # plt.plot(bl+(data_bl[0]-bl[0]))
-    # plt.clf()
-    
-    # plt.subplot(2,2,1)
-    # plt.title('BL_DATA AND FIT_DATA')
-    # plt.plot(data_bl)
-    # plt.plot(fun3)
-    # plt.subplot(2,2,2)
-    # plt.title('BL_DATA AND FIT-LORENTZ')
-    # plt.plot(data_bl)
-    # plt.plot(bl+(data_bl[0]-bl[0]))
-    # plt.subplot(2,2,3)
-    # plt.title('BL_REMOVED')
-    # plt.plot(data_bl-(bl+(np.mean(data_bl[:5])-np.mean(bl[:5]))))
-    # plt.subplot(2,2,4)
-    # plt.title('ORG AND BL_REMOVED')
-    # plt.plot(data)
-    # plt.plot(data_bl-(bl+(np.mean(data_bl[:5])-np.mean(bl[:5]))))
-    
-    plt.pause(0.5)
+    plt.plot(data,label = 'real data')
+    plt.plot(data_bl-(bl_rem+(np.mean(data_bl[:5])-np.mean(bl_rem[:5]))),label = 'data after \nbaseline removing')
+    plt.legend()
+    plt.pause(pause)
